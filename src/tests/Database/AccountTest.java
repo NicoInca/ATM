a70ef2950d9560c499b56d8a8e084c5753fb78e5
NicoInca <nicoinca00@hotmail.it>
2022-11-18 17:57:29 +0100

    creazione package Test.Database e file richiesti

---
+++ src/tests/Database/AccountTest.java
@@ -0,0 +1,39 @@
+package tests.Database;
+
+import org.junit.Test;
+
+import code.Database.Account;
+
+public class AccountTest {
+
+    @Test
+    public void testValidatePIN(){
+
+    }
+
+    @Test
+    public void testGetAvailableBalance(){
+
+    }
+    
+    @Test
+    public void testGetTotalBalance(){
+        
+    }
+
+    @Test
+    public void testCredit(){
+
+    }
+
+    @Test
+    public void testDebit(){
+
+    }
+
+    @Test
+    public void testGetAccountNumber(){
+        
+    }
+    
+}
---
+++ src/tests/Database/BankDatabaseTest.java
@@ -0,0 +1,39 @@
+package tests.Database;
+
+import org.junit.Test;
+
+import code.Database.BankDatabase;
+
+public class BankDatabaseTest {
+
+    @Test
+    public void testGetAccount(){
+
+    }
+
+    @Test
+    public void testAuthenticateUser(){
+
+    }
+
+    @Test
+    public void testGetAvailableBalance(){
+
+    }
+
+    @Test
+    public void testGetTotalBalance(){
+
+    }
+
+    @Test
+    public void testCredit(){
+
+    }
+
+    @Test
+    public void testDebit(){
+        
+    }
+    
+}

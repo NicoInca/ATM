31380d9a53073022280cf8dc5eccb37f7bda3eaf
NicoInca <nicoinca00@hotmail.it>
2022-11-18 16:02:21 +0100

    aggiunta di vari test su Euro

--- src/tests/Buisness_logic/EuroTest.java
+++ src/tests/Buisness_logic/EuroTest.java
@@ -1,6 +1,7 @@
 package tests.Buisness_logic;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -36,21 +37,26 @@ public class EuroTest {
 
     @Test
     public void testSomma() {
-
+        test = new Euro(0);
     }
 
     @Test
     public void testSottrai() {
-
+        test = new Euro(60);
+        Euro sconto = new Euro (9,99);
+        assertEquals(test.sottrai(sconto).getValore(), 5001, 0);
     }
 
     @Test
     public void testStampa() {
-
+        test = new Euro(23, 50);
+        assertEquals(test.stampa(), "23.5 euro");
     }
 
     @Test
     public void testUgualeA() {
-
+        Euro compare = new Euro(1.20);
+        test = new Euro(1, 20);
+        assertTrue(compare.ugualeA(test));
     }
 }
